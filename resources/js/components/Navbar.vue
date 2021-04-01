00bfe0b9a043fc8c9e6e9baf26a98e73ebb03bba
narita <b2560305@planet.kanazawa-it.ac.jp>
2021-04-01 09:51:18 +0000

    add upload photo form

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -5,7 +5,7 @@
         </RouterLink>
         <div class="navbar__menu">
             <div v-if="isLogin" class="navbar__item">
-                <button class="button">
+                <button class="button" @click="showForm = ! showForm">
                     <i class="icon icon-md-add">
                         Submit a photo
                     </i>
@@ -20,11 +20,22 @@
                 </RouterLink>
             </div>
         </div>
+        <PhotoForm v-model="showForm" />
     </nav>
 </template>
 
 <script>
+import PhotoForm from './PhotoForm.vue'
+
 export default {
+    components: {
+        PhotoForm
+    },
+    data(){
+        return {
+            showForm: false
+        }
+    },
     computed: {
         isLogin(){
             return this.$store.getters['auth/check']
