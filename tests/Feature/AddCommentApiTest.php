cb828e9313cd902dc435b0246655ef5c24e234d3
narita <b2560305@planet.kanazawa-it.ac.jp>
2021-04-25 13:43:23 +0000

    make AddCommentApiTest.php

---
+++ tests/Feature/AddCommentApiTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Photo;
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AddCommentApiTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void{
+        parent::setUp();
+
+        $this->user = factory(User::class)->create();
+    }
+}
